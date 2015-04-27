6b26f1e47c24505ebdc3e0e633f38fd92b8c47be
cnso <hdmj@sina.com>
2015-04-27 17:05:14 +0800

    测试提交

--- app/src/main/java/com/qianfeng/sharetest/MainActivity.java
+++ app/src/main/java/com/qianfeng/sharetest/MainActivity.java
@@ -94,6 +94,7 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
                     text.setText(builder.toString());
                 }
             });
+
         }
 
     }
